4b05766a7d3a542979c1b410107576a4d5b9b7b3
h10hUI <admin@hi0711.xyz>
2024-12-23 17:44:55 +0900

    config: ⚙️  treesitter-context の導入

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -6,3 +6,21 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   }
 }
+
+require'treesitter-context'.setup {
+  enable = true,
+  multiwindow = false,
+  max_lines = 0,
+  min_window_height = 0,
+  line_numbers = true,
+  multiline_threshold = 20,
+  trim_scope = 'outer',
+  mode = 'cursor',
+  separator = nil,
+  zindex = 20,
+  on_attach = nil,
+}
+-- Keybindings
+vim.keymap.set("n", "[h", function()
+  require("treesitter-context").go_to_context(vim.v.count1)
+end, { silent = true })
