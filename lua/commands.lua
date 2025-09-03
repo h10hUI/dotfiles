8a6eac186727330598126dc18ce53df69aa73926
h10hUI <5786454+h10hUI@users.noreply.github.com>
2025-09-03 18:13:18 +0900

    feat: 🎸 add Bw command to close other unmodified buffers

--- lua/commands.lua
+++ lua/commands.lua
@@ -12,6 +12,20 @@ local function setup()
     vim.cmd("file " .. opts.args)
     vim.cmd("call delete(expand('#'))")
   end, { nargs = 1, complete = "file" })
+
+  -- bw コマンドを current buffer 以外の全バッファで実行
+  vim.api.nvim_create_user_command("Bw", function()
+    local current_buf = vim.api.nvim_get_current_buf()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if buf ~= current_buf and vim.api.nvim_buf_is_loaded(buf) then
+        local buftype = vim.bo[buf].buftype
+        local modified = vim.bo[buf].modified
+        if buftype == '' and not modified then
+          vim.api.nvim_buf_delete(buf, {})
+        end
+      end
+    end
+  end, { desc = "Close other unmodified buffers" })
 end
 
 return { setup = setup }
