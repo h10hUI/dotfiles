7a9ae1cb36a5dbcda65544b58e884eb8b5c5138a
h10hUI <5786454+h10hUI@users.noreply.github.com>
2026-05-27 11:37:05 +0900

    config: ⚙️  update config

--- sync-config.sh
+++ sync-config.sh
@@ -4,13 +4,19 @@
 # Make ~/.config/nvim/lua read-only initially
 chmod -R a-w ~/.config/nvim/lua
 
-nohup fswatch -o ~/.config/nvim/init.lua ~/dotfiles/lua ~/.config/wezterm | \
+nohup fswatch -o \
+  ~/.config/nvim/init.lua \
+  ~/dotfiles/lua \
+  ~/.config/wezterm \
+  /Applications/Adobe Illustrator 2026/Presets.localized/ja_JP/スクリプト \
+| \
 while read; do
   rsync -r ~/.config/nvim/init.lua ~/dotfiles/init.lua
   chmod -R u+w ~/.config/nvim/lua
   rsync -r ~/dotfiles/lua ~/.config/nvim
   chmod -R a-w ~/.config/nvim/lua
   rsync -r ~/.config/wezterm ~/dotfiles
+  rsync -r /Applications/Adobe Illustrator 2026/Presets.localized/ja_JP/スクリプト ~/Library/CloudStorage/GoogleDrive-admin@hi0711.xyz/マイドライブ/ai_jsx
 done &
 
 echo "Config sync started (PID: $!)"
